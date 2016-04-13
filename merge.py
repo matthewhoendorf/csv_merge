dcb3d1352a796c9c1be63de02ba2495ef630d20e
Matt Hoendorf <hoendomd@mail.uc.edu>
2016-04-13 16:50:47 -0400

    removed attempt at windows compatibility

--- merge.py
+++ merge.py
@@ -2,9 +2,6 @@ import sys
 from sys import argv
 from glob import glob
 
-if '*' in sys.argv[-1]:
-    sys.argv[-1:] = glob.glob(sys.argv[-1])
-
 fout = open('output.csv','a') # output csv
 
 for row in open(argv[1]): # first file
